33bc894ec0f6dbc9db9ba04aac66b7307f300680
Vikrant singh <kingtvarshin1@gmail.com>
2024-07-20 20:02:15 +0530

    testing .1 sma added

--- indicator_responses.py
+++ indicator_responses.py
@@ -108,11 +108,70 @@ small_capstocksdict = json.load(small_capstocksdictfile)
 backdays=0
 lock = threading.Lock()
 
+# indicators_data = {
+#     "company":[],
+#     "cap":[],
+#     "closing_price":[],
+#     "sma":[],
+#     "sma%":[],
+#     "PE_ratio":[],
+#     "ball":[],
+#     "rsi":[],
+#     "stoch":[],
+#     "super_trend":[]
+# }
+
+# def indicator_response_dict_creator(dict_file,cap,backdays=0):
+#     # global indicators_data
+#     with lock:
+#         for key in dict_file.keys():
+#             print(key)
+#             closing_price,sma,ball,rsi,stoch,super_trend = indicators_response(key,backdays)
+#             if ball or rsi or stoch:
+#                 indicators_data["company"].append(key)
+#                 indicators_data["cap"].append(cap)
+#                 indicators_data["closing_price"].append(closing_price)
+#                 indicators_data["sma"].append(sma)
+#                 try:
+#                     indicators_data["sma%"].append(((closing_price-sma)/closing_price)*100)
+#                 except:
+#                     indicators_data["sma%"].append('')
+#                 indicators_data["PE_ratio"].append(dict_file[key]["PE_ratio"])
+#                 indicators_data["ball"].append(ball)
+#                 indicators_data["rsi"].append(rsi)
+#                 indicators_data["stoch"].append(stoch)
+#                 indicators_data["super_trend"].append(super_trend)
+        
+# t4 = threading.Thread(target=indicator_response_dict_creator, args=(large_capstocksdict,'large_cap',backdays,))
+# t5 = threading.Thread(target=indicator_response_dict_creator, args=(mid_capstocksdict,'mid_cap',backdays,))
+# t6 = threading.Thread(target=indicator_response_dict_creator, args=(small_capstocksdict,'small_cap',backdays,))
+
+# t4.start()
+# t5.start()
+# t6.start()
+
+# t4.join()
+# t5.join()
+# t6.join()
+
+# # indicator_response_dict_creator(large_capstocksdict,'large_cap',backdays)
+# # indicator_response_dict_creator(mid_capstocksdict,'mid_cap',backdays)
+# # indicator_response_dict_creator(small_capstocksdict,'small_cap',backdays)
+# indicators_df = pd.DataFrame(indicators_data)
+# # indicators_df.to_csv(f'./stock_indicators_csv/indicators_data_test_full3.csv')
+# print(indicators_df)
+
+
+
 indicators_data = {
     "company":[],
     "cap":[],
     "closing_price":[],
     "sma":[],
+    "sma100":[],
+    "sma50":[],
+    "sma20":[],
+    "sma10":[],
     "sma%":[],
     "PE_ratio":[],
     "ball":[],
@@ -122,25 +181,29 @@ indicators_data = {
 }
 
 def indicator_response_dict_creator(dict_file,cap,backdays=0):
-    # global indicators_data
-    with lock:
-        for key in dict_file.keys():
-            print(key)
-            closing_price,sma,ball,rsi,stoch,super_trend = indicators_response(key,backdays)
-            if ball or rsi or stoch:
-                indicators_data["company"].append(key)
-                indicators_data["cap"].append(cap)
-                indicators_data["closing_price"].append(closing_price)
-                indicators_data["sma"].append(sma)
-                try:
-                    indicators_data["sma%"].append(((closing_price-sma)/closing_price)*100)
-                except:
-                    indicators_data["sma%"].append('')
-                indicators_data["PE_ratio"].append(dict_file[key]["PE_ratio"])
-                indicators_data["ball"].append(ball)
-                indicators_data["rsi"].append(rsi)
-                indicators_data["stoch"].append(stoch)
-                indicators_data["super_trend"].append(super_trend)
+    global indicators_data
+    # with lock:
+    for key in dict_file.keys():
+        print(key)
+        closing_price,sma,sma100,sma50,sma20,sma10,ball,rsi,stoch,super_trend = indicators_response(key,backdays)
+        if ball or rsi or stoch:
+            indicators_data["company"].append(key)
+            indicators_data["cap"].append(cap)
+            indicators_data["closing_price"].append(closing_price)
+            indicators_data["sma"].append(sma)
+            try:
+                indicators_data["sma%"].append(((closing_price-sma)/closing_price)*100)
+            except:
+                indicators_data["sma%"].append('')
+            indicators_data["sma100"].append(sma100)
+            indicators_data["sma50"].append(sma50)
+            indicators_data["sma20"].append(sma20)
+            indicators_data["sma10"].append(sma10)
+            indicators_data["PE_ratio"].append(dict_file[key]["PE_ratio"])
+            indicators_data["ball"].append(ball)
+            indicators_data["rsi"].append(rsi)
+            indicators_data["stoch"].append(stoch)
+            indicators_data["super_trend"].append(super_trend)
         
 t4 = threading.Thread(target=indicator_response_dict_creator, args=(large_capstocksdict,'large_cap',backdays,))
 t5 = threading.Thread(target=indicator_response_dict_creator, args=(mid_capstocksdict,'mid_cap',backdays,))
@@ -154,9 +217,5 @@ t4.join()
 t5.join()
 t6.join()
 
-# indicator_response_dict_creator(large_capstocksdict,'large_cap',backdays)
-# indicator_response_dict_creator(mid_capstocksdict,'mid_cap',backdays)
-# indicator_response_dict_creator(small_capstocksdict,'small_cap',backdays)
 indicators_df = pd.DataFrame(indicators_data)
-# indicators_df.to_csv(f'./stock_indicators_csv/indicators_data_test_full3.csv')
 print(indicators_df)
